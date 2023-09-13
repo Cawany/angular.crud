9bb5b9fcf62b155fb40f4e024b2634f55ebdf76c
Cawany Fonseca <cawany.fonseca@accelera.com.br>
2023-09-13 10:59:30 -0300

    comitad10

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { HomeComponent } from "./view/home/home.component";
 import { ProductCrudComponent } from "./view/product-crud/product-crud.component";
 import { ProductCreateComponent } from './components/product/product-create/product-create.component';
 import { ProductUpdateComponent } from './components/product/product-update/product-update.component';
+import { ProductDeleteComponent } from './components/product/product-delete/product-delete.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: "products/update/:id",
     component: ProductUpdateComponent
+  },
+  {
+    path: "products/delete/:id",
+    component: ProductDeleteComponent
   }
 ];
 
--- frontend/src/app/components/product/product.service.ts
+++ frontend/src/app/components/product/product.service.ts
@@ -1,27 +1,24 @@
 import { Product } from './product.model';
 import { Injectable } from '@angular/core';
-import { MatSnackBar } from'@angular/material/snack-bar';
-import { HttpClient } from'@angular/common/http' 
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
-
-
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ProductService {
-
- public baseUrl: string = "http://localhost:3001/products"
+  public baseUrl: string = 'http://localhost:3001/products';
   /*public baseUrl: string = "http://localhost:3001/products"*/
 
-  constructor(private snackBar: MatSnackBar, private http: HttpClient) { }
+  constructor(private snackBar: MatSnackBar, private http: HttpClient) {}
 
   showMessage(msg: string): void {
     this.snackBar.open(msg, 'X', {
       duration: 3000,
-      horizontalPosition: "right",
-      verticalPosition: "top"
-    })
+      horizontalPosition: 'right',
+      verticalPosition: 'top',
+    });
   }
 
   /*create(product: Product):Observable<Product> {
@@ -29,28 +26,27 @@ export class ProductService {
     observable faz a checagem de tipo, no caso Product
   }*/
 
- create(product: Product): Observable<Product> {
-  return this.http.post<Product>(this.baseUrl, product)
- }
- /*vai retornar um Observable e dentro desse Observable 
+  create(product: Product): Observable<Product> {
+    return this.http.post<Product>(this.baseUrl, product);
+  }
+  /*vai retornar um Observable e dentro desse Observable 
  vai retornar uma lista de produto */
- read(): Observable<Product[]> {
-  return this.http.get<Product[]>(this.baseUrl)
- }
+  read(): Observable<Product[]> {
+    return this.http.get<Product[]>(this.baseUrl);
+  }
 
- readById(id: String):Observable<Product> {
-  const url = `${this.baseUrl}/${id}`
-  return this.http.get<Product>(url)
- }
+  readById({ id }: { id: String; }): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.get<Product>(url);
+  }
 
- update(product: Product):Observable<Product> {
-  const url = `${this.baseUrl}/${product.id}`
-  return this.http.put<Product>(url, product)
- }
+  update(product: Product): Observable<Product> {
+    const url = `${this.baseUrl}/${product.id}`;
+    return this.http.put<Product>(url, product);
+  }
 
- /*delete(product: Product): Observable<Product> {
-  const url = `${this.baseUrl}/${product.id}`
-  return this.http.delete<Product>(url, product)
- }*/
-
+  delete(id: number): Observable<Product> {
+    const url = `${this.baseUrl}/${id}`;
+    return this.http.delete<Product>(url);
+  }
 }
